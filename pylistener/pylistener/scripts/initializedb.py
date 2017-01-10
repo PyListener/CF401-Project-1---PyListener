66666a4f2ab102e6810c5d70078b5203544e1db9
Ted Callahan <CCallahanIV@gmail.com>
2017-01-09 17:42:31 -0800

    uncommented Categories in import statement.

--- pylistener/pylistener/scripts/initializedb.py
+++ pylistener/pylistener/scripts/initializedb.py
@@ -15,7 +15,7 @@ from ..models import (
     get_session_factory,
     get_tm_session,
     )
-from ..models import User, AddressBook, Attributes#, Categories
+from ..models import User, AddressBook, Attributes, Categories
 
 
 def usage(argv):
