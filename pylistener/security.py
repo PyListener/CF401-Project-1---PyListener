01a0ee1922de23133bbe682cacedfea341e6af0d
Maelle Vance <maellevance@gmail.com>
2017-01-11 22:38:24 -0800

    functional tests

--- pylistener/security.py
+++ pylistener/security.py
@@ -37,4 +37,4 @@ def includeme(config):
     session_secret = os.environ.get("SESSION_SECRET", "itsaseekrit")
     session_factory = SignedCookieSessionFactory(session_secret)
     config.set_session_factory(session_factory)
-    config.set_default_csrf_options(require_csrf=True)
+    # config.set_default_csrf_options(require_csrf=True)
